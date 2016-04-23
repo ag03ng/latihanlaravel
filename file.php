039755c70e58d4526614d6a9a26cbe849852d276
ag03ng <ag03ngprasetyo@gmail.com>
2016-04-23 21:02:48 +0700

    commit 2

---
+++ file.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="log/cek.php" method="post">
+<input name="user" type="text"><br>
+
+</input>	
+<input name="pass" type="password"><br>
+
+</input>	
+<input name="submit" type="submit"><br>
+
+</input>	
+
+
+</form>
+</body>
+</html>
---
+++ titit.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php  
+include'../koneksi/koneksi.php';
+$user=$_POST['user'];
+$pass=$_POST['pass'];
+// $pw=md5($pass);
+
+$sql=$koneksi-> query(" SELECT * from admin where id_admin='$user' and password='$pass' ") or die (mysql_error());
+$cari=mysqli_num_rows($sql);
+if($cari){
+	$_SESSION['user']=$user;
+?>
+<script type="text/javascript">
+	document.location='branda.php';
+	alert('tidak');
+</script>
+<?php
+?>
+}else{ 
+?>
+<script type="text/javascript">
+	document.location='index.php';
+	alert('berhasild');
+</script>
+<?php	
+}
+?>
+</body>
+</html>
